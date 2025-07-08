1a2d15140971744f47e7ee5e852d3fb5045f09e4
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-07-08 12:00:56 +0000

    search

--- public/search.php
+++ public/search.php
@@ -1,57 +1,4 @@
 <?php require_once realpath(__DIR__ . '/../private/src/init.php');
-
-function search()
-{
-	if (!isset($_GET['query']) || !isset($_GET['type'])) {
-		return null;
-	}
-
-	$db_address = $_ENV['mysql_address'];
-	$db_username = $_ENV['mysql_username'];
-	$db_password = $_ENV['mysql_password'];
-	$db_name = $_ENV['mysql_db_name'];
-	$is_connection = false;
-	$is_stmt = false;
-
-	$search_query = $_GET['query'];
-	$search_type = $_GET['type'];
-
-
-	try {
-		$connection = mysqli_connect($db_address, $db_username, $db_password, $db_name);
-		$is_connection = true;
-		$query = 'select * from(select title, body, user_id, levenshtein(lower(title), lower(?)) as dist from posts ) as sub where dist <= 3 order by dist asc limit 10';
-		$stmt = mysqli_prepare($connection, $query);
-		$is_stmt = true;
-		mysqli_stmt_bind_param($stmt, 's', $search_query);
-		mysqli_stmt_execute($stmt);
-
-		mysqli_stmt_bind_result($stmt, $title, $body, $user_id, $dist);
-
-		while (mysqli_stmt_fetch($stmt)) {
-			error_log("error: $title");
-			$search_results[] = [
-				'title' => $title,
-				'body' => $body,
-				'user_id' => $user_id,
-			];
-		}
-	} catch (mysqli_sql_exception $e) {
-		if ($is_stmt) {
-			mysqli_stmt_close($stmt);
-		}
-		if ($is_connection) {
-			mysqli_close($connection);
-		}
-		$_SESSION['msgs'][] = "Database Error: $e";
-		header('Location: /search.php');
-		exit;
-	}
-
-	mysqli_stmt_close($stmt);
-	mysqli_close($connection);
-	return $search_results;
-}
 ?>
 <html>
 
@@ -77,10 +24,20 @@ function search()
 			<button type="submit">Submit!</button>
 		</form>
 
-		<?php $search_results = search(); ?>
-		<?php if ($search_results !== null): ?>
+		<?php
+		if (isset($_GET['query']) && isset($_GET['type'])) {
+			require_once realpath(__root_dir . '/private/src/search.php');
+			if ($_GET['type'] === 'post') {
+				$search_results_posts = search_posts($_GET['query']);
+			}
+			if ($_GET['type'] === 'user') {
+				$search_results_users = search_users($_GET['query']);
+			}
+		}
+		?>
+		<?php if (isset($search_results_posts) && count($search_results_posts)): ?>
 			<div id="posts_container">
-				<?php foreach ($search_results as $result): ?>
+				<?php foreach ($search_results_posts as $result): ?>
 					<div id="post">
 						<p>Title: <?php echo $result['title']; ?></p>
 						<p>User_id: <?php echo $result['user_id']; ?></p>
