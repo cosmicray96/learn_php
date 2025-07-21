4e9ac221ce77351a67a5d9f54cf73edbaea263fd
cosmicray96 <cosmicrayleigh02@gmail.com>
2025-07-21 06:43:45 +0000

    better comments

---
+++ private/_common/view/comment/full.php
@@ -0,0 +1,16 @@
+<?php function render_comment($comment)
+{ ?>
+	<div class="comment_full">
+		<div class="comment_parent_full">
+			<p>Username: <?php echo $comment['username']; ?></p>
+			<p>Created At: <?php echo $comment['created_at']; ?></p>
+			<p>Body: <?php echo $comment['body']; ?></p>
+		</div>
+
+		<div class="comment_children_full">
+			<?php foreach ($comment['children'] as $child): ?>
+				<?php render_comment($child); ?>
+			<?php endforeach; ?>
+		</div>
+	</div>
+<?php } ?>
--- private/posts/id/view.php
+++ private/posts/id/view.php
@@ -1,5 +1,6 @@
 <?php
 
+/*
 function render_comments(array &$comments): void
 {
 	foreach ($comments as $comment) {
@@ -18,13 +19,13 @@ function render_comments(array &$comments): void
 	}
 }
 
+*/
 ?>
 
 <?php require __root_dir . '/private/_common/view/post/full.php'; ?>
-
-<!-- comments -->
+<?php require_once __root_dir . '/private/_common/view/comment/full.php'; ?>
 <div class="comments_container">
-	<?php if (isset($comments)): ?>
-		<?php render_comments($comments); ?>
-	<?php endif; ?>
+	<?php foreach ($comments as $comment): ?>
+		<?php render_comment($comment); ?>
+	<?php endforeach; ?>
 </div>
